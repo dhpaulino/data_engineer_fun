14887965469f384603cb8bae691ea769c26f57c0
Davison Paulino <dhpaulino@gmail.com>
2020-05-30 01:14:16 -0300

    feat: add data pipeline project
    * add main files
    * implement copy s3 to redshift

---
+++ data_pipeline_sparkify/dags/songplays_S3_to_DWH.py
@@ -0,0 +1,55 @@
+from datetime import datetime, timedelta
+from airflow.models import Variable
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators import (StageToRedshiftOperator, LoadFactOperator,
+                                LoadDimensionOperator, DataQualityOperator)
+
+from helpers import SqlQueries
+
+dag = DAG('songplays_S3_to_DWH',
+          description='Load and transform data in Redshift with Airflow',
+          schedule_interval=None,
+          start_date=datetime(2020, 5, 29),
+          default_args={
+              "owner": "dhpaulino"
+          }
+        )
+
+
+# TODO: create/check/empty tables before the process
+'''
+CREATE TABLE IF NOT EXISTS public.staging_events(
+	artist varchar(256),
+	auth varchar(256),
+	firstname varchar(256),
+	gender varchar(256),
+	iteminsession int4,
+	lastname varchar(256),
+	length numeric(18,0),
+	"level" varchar(256),
+	location varchar(256),
+	"method" varchar(256),
+	page varchar(256),
+	registration numeric(18,0),
+	sessionid int4,
+	song varchar(256),
+	status int4,
+	ts int8,
+	useragent varchar(256),
+	userid int4
+);
+
+TRUNCATE  public.staging_events;
+'''
+# FIXME: no data is loaded at staging_events table
+stage_events_to_redshift = StageToRedshiftOperator(
+    task_id='Stage_events',
+    redshift_conn_id="redshift_conn",
+    arn_iam_role=Variable.get("redshift_s3_role"),
+    output_table="staging_events",
+    s3_bucket="udacity-dend",
+    s3_key="log_data",
+    copy_parameters="json 's3://udacity-dend/log_json_path.json' timeformat as 'epochmillisecs'",
+    dag=dag
+)
---
+++ data_pipeline_sparkify/plugins/operators/stage_redshift.py
@@ -0,0 +1,33 @@
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
+from airflow.hooks.postgres_hook import PostgresHook
+
+class StageToRedshiftOperator(BaseOperator):
+    """ Copies the data from a given s3 location to a table in redshift"""
+    ui_color = '#358140'
+
+    @apply_defaults
+    def __init__(self, redshift_conn_id, arn_iam_role, output_table, s3_bucket, s3_key, copy_parameters,
+                 *args, **kwargs):
+
+        super(StageToRedshiftOperator, self).__init__(*args, **kwargs)
+        self.redshift_conn_id = redshift_conn_id
+        self.arn_iam_role = arn_iam_role
+        self.output_table = output_table
+        self.s3_bucket = s3_bucket
+        self.s3_key = s3_key
+        self.copy_parameters = copy_parameters
+
+    def execute(self, context):
+        self.hook = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        copy_sql = f"copy {self.output_table} from 's3://{self.s3_bucket}/{self.s3_key}'\
+                    iam_role '{self.arn_iam_role}' {self.copy_parameters}"
+        self.log.info('Executing COPY command...')
+        self.logger.info(copy_sql)
+        self.hook.run(copy_sql)
+
+
+
+
+
